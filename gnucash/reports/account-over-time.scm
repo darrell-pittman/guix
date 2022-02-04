e49ae69c38b4bbd2c12cab276a7f2b75ec8b553f
Darrell Pittman <darrellppittman@gmail.com>
2022-02-04 16:10:14 -0330

    Added table

--- gnucash/reports/account-over-time.scm
+++ gnucash/reports/account-over-time.scm
@@ -19,6 +19,7 @@
 (define optname-account (N_ "Account"))
 (define optname-num-years (N_ "Num Years"))
 (define optname-delta (N_ "Sample Delta"))
+(define optname-show-table (N_ "Show Table"))
 
 (define (options-generator)
   (let* ([options (gnc:new-options)]
@@ -43,7 +44,7 @@
      (gnc:make-number-range-option
       gnc:pagename-display optname-num-years
       "ee" (N_ "Number of years to report.")
-      2     ;; default
+      1     ;; default
       1     ;; lower bound
       3    ;; upper bound
       0     ;; number of decimals
@@ -58,9 +59,25 @@
       'MonthDelta
       (list (vector 'MonthDelta (N_ "Month"))
             (vector 'WeekDelta (N_ "Week")))))
+
+    (add-option
+     (gnc:make-simple-boolean-option
+      gnc:pagename-display optname-show-table
+      "a" (N_ "Show values in table.") #t))
     
     (gnc:options-set-default-section options gnc:pagename-display)
     options))
+
+(define (make-value-cell val)
+  (let ([cell (gnc:make-html-table-cell
+               (gnc:make-html-text
+                (format #f "~1,2f" val)))])
+    (gnc:html-table-cell-set-style!
+     cell
+     "td"
+     'attribute
+     '("style" "text-align:right"))
+    cell))
 
 (define (account-over-time-renderer report-obj)
   (define (get-op section name)
@@ -71,8 +88,10 @@
 
   (let* ([document (gnc:make-html-document)]
          [chart (gnc:make-html-chart)]
+         [table (gnc:make-html-table)]
          [accounts (op-value gnc:pagename-display optname-account)]
          [num-years (op-value gnc:pagename-display optname-num-years)]
+         [show-table (op-value gnc:pagename-display optname-show-table)]
          [delta (op-value gnc:pagename-display optname-delta)]
          [extents (wgc:prev-year-extents delta num-years)]
          [colours (wgc:random-colours (length accounts))])
@@ -85,6 +104,15 @@
 
     (gnc:html-chart-set-y-axis-label! chart "Amount")
 
+    (gnc:html-table-set-style!
+     table "table"
+     'attribute (list "border" 1)
+     'attribute (list "width" "") 
+     'attribute (list "align" "center")
+     'attribute (list "valign" "top")
+     'attribute (list "cellspacing" 0)
+     'attribute (list "cellpadding" 5))
+    
     (let loop ([accounts accounts]
                [iter 0]
                [colours colours])
@@ -100,24 +128,48 @@
               (gnc:html-chart-set-currency-symbol!
                chart
                (gnc-commodity-get-nice-symbol curr))
-              
-              (gnc:html-chart-set-data-labels!
-               chart
-               (wgc:account-trend-get-data-labels trend))))
 
-          (gnc:html-chart-add-data-series!
-           chart
-           (wgc:account-name acct)
-           (wgc:account-trend-get-values trend)
-           (car colours)
-           'border-width 1
-           'fill #f)
+              (let ([labels (wgc:account-trend-get-data-labels trend)])
+                (gnc:html-chart-set-data-labels!
+                 chart
+                 labels)
+
+
+                (gnc:html-table-append-row!
+                 table
+                 (map (lambda (lbl)
+                        (gnc:make-html-table-header-cell
+                         (gnc:make-html-text
+                          (gnc:html-markup-b lbl))))
+                      (append (cons "Account" labels) '("Total")))))))
+
+          (let* ([vals (wgc:account-trend-get-values trend)]
+                 [total (apply + vals)])
+            
+            (gnc:html-chart-add-data-series!
+             chart
+             (wgc:account-name acct)
+             (wgc:account-trend-get-values trend)
+             (car colours)
+             'border-width 1
+             'fill #f)
+
+            (gnc:html-table-append-row!
+             table
+             (cons (gnc:make-html-table-cell
+                    (gnc:make-html-text
+                     (gnc:html-markup-b (wgc:account-name acct))))
+                   (map make-value-cell
+                        (append vals (list total))))))
 
           (loop (cdr accounts)
                 (1+ iter)
                 (cdr colours)))))
     
     (gnc:html-document-add-object! document chart)
+    
+    (when show-table
+      (gnc:html-document-add-object! document table))
     
     document))
 
